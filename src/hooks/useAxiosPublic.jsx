88e1b3ff8e816f0fd5b77d8577468a754611efbd
hosainahmed <hosainahmed534745@gmail.com>
2024-09-25 04:28:35 +0600

    vercel deploy

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bistro-boss-server-ptbe4md5u-hosain-alis-projects.vercel.app",
 });
 
 const useAxiosPublic = () => {
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -3,16 +3,6 @@ import { useQuery } from "@tanstack/react-query";
 
 const useMenu = () => {
   const axiosPublic =  useAxiosPublic();
-  // const [menu, setMenu] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   axiosSecure.get("/menu")
-  //     .then((res) => {
-  //       setLoading(false);
-  //       setMenu(res.data);
-  //     });
-  // }, []);
-
   const {data: menu = [], isPending: loading, refetch} = useQuery({
     queryKey: ['menu'], 
     queryFn: async() =>{
